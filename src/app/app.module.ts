30862804f6b4b83bc95f93b811a453fd22d9545f
jitendrabagul21 <jitendrabagul21@gmail.com>
2019-04-03 13:12:46 +0530

    4 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+ 
+import { AppfooterComponent } from './appfooter/appfooter.component';
+import { AppheaderComponent } from './appheader/appheader.component';
+import { AppmenuComponent } from './appmenu/appmenu.component';
+import { AppsettingsComponent } from './appsettings/appsettings.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,  AppfooterComponent, AppheaderComponent, AppmenuComponent, AppsettingsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
